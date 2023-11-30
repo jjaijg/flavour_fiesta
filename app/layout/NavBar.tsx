8c1a780b54e275885160ca3bef7c148db1239fc3
ipsitapriyadarsini <41966804+ipsitapriyadarsini@users.noreply.github.com>
2023-11-30 10:33:07 +0530

    added layout folder and navbar

---
+++ app/layout/NavBar.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+import React from "react";
+
+const NavBar = () => {
+  const menus = ["HOME", "ABOUT", "ITEMS"];
+  return (
+    <nav className="flex p-5 shadow-md">
+      <Link href="./">Flavour Fiesta</Link>
+      <ul className="flex flex-1 justify-end mr-20">
+        {menus.map((menu, i) => (
+          <li className="px-5  " key={i}>
+            <Link href="/home">{menu}</Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
