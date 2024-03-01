af1a47026aae044a54beae259656fc30e1c6bfac
ipsitapriyadarsini <41966804+ipsitapriyadarsini@users.noreply.github.com>
2024-03-01 09:52:42 +0530

    Ran next and express server with npm run dev

--- server/app.ts
+++ server/app.ts
@@ -1,28 +1,43 @@
 import dotenv from "dotenv";
 import express, { Express, Request, Response } from "express";
+import next from "next";
 import mongoose from "mongoose";
+import recipeRouter from "./routes/recipes";
 
 dotenv.config();
-const app: Express = express();
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
 
-const mongoDbUrl: string = process.env.MONGODB_URL as string;
-mongoose.connect(mongoDbUrl);
-const db = mongoose.connection;
+const handle = app.getRequestHandler();
 
-db.on("open", () => {
-  console.log("connected...");
-});
+app.prepare().then(() => {
+  const server = express();
+  const mongoDbUrl: string = process.env.MONGODB_URL as string;
+  mongoose.connect(mongoDbUrl);
+  const db = mongoose.connection;
 
-// Handle MongoDB connection error
-db.on("error", (error) => {
-  console.error("Error connecting to MongoDB:", error);
-});
+  db.on("open", () => {
+    console.log("connected...");
+  });
 
-app.use(express.json());
+  // Handle MongoDB connection error
+  db.on("error", (error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
 
-import recipeRouter from "./routes/recipes";
-app.use("/recipes", recipeRouter);
+  // Body parser middleware
+  server.use(express.json());
 
-app.listen(3000, () => {
-  console.log("server started");
+  // Import API routes
+  server.use("/recipes", recipeRouter);
+
+  // Next.js page handling
+  server.get("*", (req, res) => handle(req, res));
+
+  const PORT = process.env.PORT || 3000;
+  server.listen(PORT, () => {
+    // if (err) throw err;
+    console.log(`> Ready on http://localhost:${PORT}`);
+    console.log("server started");
+  });
 });
