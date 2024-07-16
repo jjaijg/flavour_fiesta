51b407652debb69fdc04611aa67bead06781cda5
ipsitapriyadarsini <41966804+ipsitapriyadarsini@users.noreply.github.com>
2024-07-16 22:20:38 +0530

    Craeted a dropdown menu component

--- components/ui/DropDown.tsx
+++ components/ui/DropDown.tsx
@@ -1,5 +1,40 @@
-import React from "react";
+import Link from 'next/link';
+import React from 'react';
 
-export const DropDown = () => {
-  return <div>DropDown</div>;
+interface Recipe {
+  name: string;
+  path: string;
+}
+
+interface DropDownProps {
+  isOpen: boolean;
+}
+
+const DropDown: React.FC<DropDownProps> = ({ isOpen }) => {
+  const recipes: Recipe[] = [
+    { name: 'Mexican', path: '/recipes/mexican' },
+    { name: 'Italian', path: '/recipes/italian' },
+    { name: 'Chinese', path: '/recipes/chinese' },
+    { name: 'Indian', path: '/recipes/indian' },
+    { name: 'German', path: '/recipes/german' },
+    { name: 'Greek', path: '/recipes/greek' },
+    { name: 'Filipino', path: '/recipes/filipino' },
+    { name: 'Japanese', path: '/recipes/japanese' },
+    { name: 'American', path: '/recipes/american' },
+  ];
+  return (
+    <>
+      {isOpen && (
+        <ul className="absolute top-full left-0 bg-white shadow-lg rounded-md mt-2 z-10">
+          {recipes.map((recipe, index) => (
+            <li className="px-4 py-2 hover:bg-gray-200" key={index}>
+              <Link href={recipe.path}>{recipe.name}</Link>
+            </li>
+          ))}
+        </ul>
+      )}
+    </>
+  );
 };
+
+export default DropDown;
--- layout/NavBar.tsx
+++ layout/NavBar.tsx
@@ -3,43 +3,65 @@ import Link from 'next/link';
 import Image from 'next/image';
 import logo from '@/assets/images/logo.png';
 import Button from '@/components/inputs/Button';
-import { handleSignOut } from '@/utilities/serverAction';
 import { useSession } from 'next-auth/react';
+import { useState } from 'react';
+import { handleSignOut } from '@/utilities/serverAction';
+import { BiSolidDownArrow, BiSolidUpArrow } from 'react-icons/bi';
+import DropDown from '@/components/ui/DropDown';
+
+interface IMenuItems {
+  name: string;
+  dropDown?: boolean;
+  path?: string;
+}
+
+const menusItems: IMenuItems[] = [
+  { name: 'about', path: '/about' },
+  { name: 'recipes', dropDown: true },
+  { name: 'cuisins', path: '/cuisins' },
+  { name: 'kitchen tips', path: '/kitchen-tips' },
+  { name: 'day plans', path: '/day-plans' },
+];
 
 const NavBar = () => {
-  // const session = await auth();
   const { data: session, status } = useSession();
+  const [dropdown, setDropdown] = useState<string | null>(null);
 
-  const menus = [
-    { name: 'about', path: '/about' },
-    { name: 'recipes', path: '/recipes' },
-    { name: 'cuisins', path: '/cuisins' },
-    { name: 'kitchen tips', path: '/kitchen-tips' },
-    { name: 'day plans', path: '/day-plans' },
-  ];
-
-  const cuisins = [
-    'Mexican',
-    'Italian',
-    'Chinese',
-    'Indian',
-    'German',
-    'Greek',
-    'Filipino',
-    'Japanese',
-    'American',
-  ];
+  const handleDropdownClick = (itemName: string) => {
+    setDropdown(dropdown === itemName ? null : itemName);
+  };
 
   return (
-    <nav className="flex items-center justify-center p-5 shadow-md uppercase font-medium bg-transparent">
-      <Link href="./" className="flex gap-2">
-        <Image src={logo} width={30} height={30} alt="Picture of logo" />
+    <nav className="flex items-center justify-between p-5 shadow-md uppercase font-medium bg-transparent">
+      <Link href="/" className="flex gap-2">
+        <Image src={logo} width={30} height={30} alt="Logo" />
         <span>Flavour Fiesta</span>
       </Link>
-      <ul className="flex flex-1 justify-end mr-20">
-        {menus.map((menu, i) => (
-          <li className="px-5" key={i}>
-            <Link href={menu.path}>{menu.name}</Link>
+      <ul className="flex flex-1 justify-end">
+        {menusItems.map((item, index) => (
+          <li
+            key={index}
+            className={`${item.name === 'recipes' ? 'relative pr-1' : 'px-5'}`}
+          >
+            {item.dropDown ? (
+              <>
+                <span onClick={() => handleDropdownClick(item.name)}>
+                  {item.name}
+                  {item.name === 'recipes' &&
+                    (dropdown ? (
+                      <BiSolidUpArrow className="inline ml-2" />
+                    ) : (
+                      <BiSolidDownArrow className="inline ml-2" />
+                    ))}
+                </span>
+
+                {dropdown === item.name && (
+                  <DropDown isOpen={dropdown === item.name} />
+                )}
+              </>
+            ) : (
+              <Link href={item.path || '#'}>{item.name}</Link>
+            )}
           </li>
         ))}
       </ul>
