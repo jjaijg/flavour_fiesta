309d045986a6e5f021793350a74015b80360beac
Jai Ganesh J <jjaiganeshjg95@gmail.com>
2024-08-15 10:10:22 +0530

    Added logic to check & add roles to the user if not present.

--- app/api/auth/login/auth.config.ts
+++ app/api/auth/login/auth.config.ts
@@ -1,35 +1,49 @@
 // /** @type {import('next').NextConfig} */
-import { Adapter } from "next-auth/adapters";
-import { MongoDBAdapter } from "@auth/mongodb-adapter";
-import clientPromise from "@/database";
-import type { NextAuthConfig } from "next-auth";
+import { Adapter } from 'next-auth/adapters';
+import { MongoDBAdapter } from '@auth/mongodb-adapter';
+import clientPromise from '@/database';
+import type { NextAuthConfig } from 'next-auth';
+import dbConnect from '@/database/db';
+import User from '@/database/model/user.model';
+import Role from '@/database/model/role.model';
 
-const authUrls = ["/login", "signup"];
+const authUrls = ['/login', 'signup'];
 
 export const authConfig = {
   pages: {
-    signIn: "/login",
+    signIn: '/login',
   },
   adapter: <Adapter>MongoDBAdapter(clientPromise),
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
-      const isOnDashboard = nextUrl.pathname.startsWith("/");
+      const isOnDashboard = nextUrl.pathname.startsWith('/');
       if (isOnDashboard) {
         if (isLoggedIn) return true;
         return false; // Redirect unauthenticated users to login page
       } else if (isLoggedIn) {
         const { pathname } = nextUrl;
         if (authUrls.includes(pathname))
-          return Response.redirect(new URL("/", nextUrl));
+          return Response.redirect(new URL('/', nextUrl));
 
         return true;
         // Response.redirect(new URL(nextUrl));
       }
       return true;
     },
-    jwt({ token, user }) {
-      if (user) token.user = user;
+    async jwt({ token, user }) {
+      await dbConnect();
+
+      if (user) {
+        const userData = await User.findById(user.id);
+        if (userData.roles.length === 0) {
+          const userRole = await Role.findOne({ name: 'user' });
+          // userData.password = 'pass';
+          userData.roles.push(userRole);
+          await userData.save();
+        }
+        token.user = { ...user, roles: userData.roles };
+      }
       return token;
     },
     session({ session, token }) {
@@ -38,7 +52,7 @@ export const authConfig = {
     },
   },
   providers: [],
-  debug: process.env.NODE_ENV === "development",
+  debug: process.env.NODE_ENV === 'development',
 } satisfies NextAuthConfig;
 
 export default authConfig;
