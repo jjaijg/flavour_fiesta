65232b9bdf47d119183f06e16d3d5a40389bc54f
Jai Ganesh J <jjaiganeshjg95@gmail.com>
2024-08-15 10:09:02 +0530

    Added logic to delete all account created for user via gauth while sign-out

--- utilities/serverAction.ts
+++ utilities/serverAction.ts
@@ -1,7 +1,16 @@
 'use server';
 
-import { signOut } from '@/app/api/auth/login/auth';
+import { signOut, auth } from '@/app/api/auth/login/auth';
+import dbConnect from '@/database/db';
+import Account from '@/database/model/account.model';
 
 export const handleSignOut = async () => {
+  const session = await auth();
+
+  await dbConnect();
+
+  // Deletes the accounts created for gauth during sign-out
+  await Account.deleteMany({ userId: session?.user.id });
+
   await signOut();
 };
